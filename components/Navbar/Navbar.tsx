9390495b21780884834b9052faebbf5572476252
naji-alhusami <najihussami@gmail.com>
2023-08-17 18:45:11 +0300

    style the icon project with title

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,24 +9,36 @@ import classes from "./Navbar.module.css";
 const Navbar: React.FC = () => {
   return (
     <nav className={classes.Navbar}>
+      <ul className={classes.navbarList}>
+        <li>
+          <Link href="/standings">HOME</Link>
+        </li>
+        <li>
+          <Link href="/standings">CONTACT</Link>
+        </li>
+        <li>
+          <Link href="/standings">ABOUT</Link>
+        </li>
+      </ul>
       <div className={classes["icon-title"]}>
-        <Image src={appIcon} alt="logo" />
-        <h1 className={classes.header}>Food2Door</h1>
+        <div className={classes["icon-title-content"]}>
+          <Image src={appIcon} alt="logo" />
+          <h1 className={classes.header}>Food2Door</h1>
+        </div>
       </div>
       <div>
         <ul className={classes.navbarList}>
           <li>
             <Link href="/standings">HOME</Link>
           </li>
-          <li>
-            <Link href="/standings">Standings</Link>
+          {/* <li>
+            <Link href="/standings">CONTACT</Link>
           </li>
           <li>
-            <Link href="/standings">Standings</Link>
-          </li>
+            <Link href="/standings">ABOUT</Link>
+          </li> */}
           <li>
             <BasketButton />
-            {/* <Link href="/standings">Standings</Link> */}
           </li>
         </ul>
       </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          {/* Add the Google Fonts link here */}
+          <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Zen+Dots&display=swap" rel="stylesheet" />
+        </Head>
       <body>
         <Main />
         <NextScript />

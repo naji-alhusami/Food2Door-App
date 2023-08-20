2781640eaf68984488f3375f2db9adfe4df7e00d
naji-alhusami <najihussami@gmail.com>
2023-08-20 14:48:04 +0300

    continue working on navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,39 +9,35 @@ import classes from "./Navbar.module.css";
 const Navbar: React.FC = () => {
   return (
     <nav className={classes.Navbar}>
-      <ul className={classes.navbarList}>
-        <li>
-          <Link href="/standings">HOME</Link>
-        </li>
-        <li>
-          <Link href="/standings">CONTACT</Link>
-        </li>
-        <li>
-          <Link href="/standings">ABOUT</Link>
-        </li>
-      </ul>
-      <div className={classes["icon-title"]}>
-        <div className={classes["icon-title-content"]}>
-          <Image src={appIcon} alt="logo" />
-          <h1 className={classes.header}>Food2Door</h1>
-        </div>
-      </div>
-      <div>
         <ul className={classes.navbarList}>
           <li>
             <Link href="/standings">HOME</Link>
           </li>
-          {/* <li>
+          <li>
+            <Link href="/standings">CONTACT</Link>
+          </li>
+          <li>
+            <Link href="/standings">ABOUT</Link>
+          </li>
+        </ul>
+        <div className={classes["icon-title"]}>
+          <Image src={appIcon} alt="logo" />
+          <h1 className={classes.header}>Food2Door</h1>
+        </div>
+        <div>
+          <ul className={classes.navbarList}>
+            <li>{/* <Link href="/standings">HOME</Link> */}</li>
+            {/* <li>
             <Link href="/standings">CONTACT</Link>
           </li>
           <li>
             <Link href="/standings">ABOUT</Link>
           </li> */}
-          <li>
-            <BasketButton />
-          </li>
-        </ul>
-      </div>
+            <li>
+              <BasketButton />
+            </li>
+          </ul>
+        </div>
     </nav>
   );
 };

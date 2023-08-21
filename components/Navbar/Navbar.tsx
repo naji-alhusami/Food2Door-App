0d0b8651fff71709f4667c860cc8d8e3bf79179b
naji-alhusami <najihussami@gmail.com>
2023-08-21 13:33:45 +0300

    about to finish styling the navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import classes from "./Navbar.module.css";
 const Navbar: React.FC = () => {
   return (
     <nav className={classes.Navbar}>
+      <div className={classes.navbarContent}>
         <ul className={classes.navbarList}>
           <li>
             <Link href="/standings">HOME</Link>
@@ -20,24 +21,14 @@ const Navbar: React.FC = () => {
             <Link href="/standings">ABOUT</Link>
           </li>
         </ul>
-        <div className={classes["icon-title"]}>
-          <Image src={appIcon} alt="logo" />
-          <h1 className={classes.header}>Food2Door</h1>
-        </div>
-        <div>
-          <ul className={classes.navbarList}>
-            <li>{/* <Link href="/standings">HOME</Link> */}</li>
-            {/* <li>
-            <Link href="/standings">CONTACT</Link>
-          </li>
-          <li>
-            <Link href="/standings">ABOUT</Link>
-          </li> */}
-            <li>
-              <BasketButton />
-            </li>
-          </ul>
-        </div>
+      </div>
+      <div className={classes["icon-title"]}>
+        <Image src={appIcon} alt="logo" />
+        <h1 className={classes.header}>Food2Door</h1>
+      </div>
+      <div className={classes.navbarButton}>
+        <BasketButton />
+      </div>
     </nav>
   );
 };

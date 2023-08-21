f55f96239410bf5059818729445ca1a68f0a3895
naji-alhusami <najihussami@gmail.com>
2023-08-21 16:13:37 +0300

    finished styling the search input and button of the home  page

--- components/HomeBackground/HomeBackground.tsx
+++ components/HomeBackground/HomeBackground.tsx
@@ -11,7 +11,13 @@ const HomeBackground = () => {
         <Image src={bgImage} alt="foodImage" />
       </div>
       <div className={classes.location}>
-        <h1>Simplify your life with doorstep food delivery using Food2Door App!</h1>
+        <h1>
+          Simplify your life with doorstep food delivery using Food2Door App!
+        </h1>
+        <div className={classes["input-container"]}>
+          <input type="text" placeholder="Enter your location" className={classes["search-input"]} />
+          <button className={classes["search-button"]}>Search</button>
+        </div>
       </div>
     </div>
   );

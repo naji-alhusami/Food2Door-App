cd1564930879f27f5ce5a63dd411be91d290d8ce
naji-alhusami <najihussami@gmail.com>
2023-08-21 14:56:00 +0300

    added background to the home page

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 import BasketButton from "./BasketButton";
-import appIcon from "@/public/Images/app-icon.png";
+import appIcon from "@/public/Images/appIcon.png";
+import bgImage from "@/public/Images/bgImage.jpg";
 import classes from "./Navbar.module.css";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className={classes.Navbar}>
-      <div className={classes.navbarContent}>
-        <ul className={classes.navbarList}>
-          <li>
-            <Link href="/standings">HOME</Link>
-          </li>
-          <li>
-            <Link href="/standings">CONTACT</Link>
-          </li>
-          <li>
-            <Link href="/standings">ABOUT</Link>
-          </li>
-        </ul>
+    <Fragment>
+      <nav className={classes.Navbar}>
+        <div className={classes.navbarContent}>
+          <ul className={classes.navbarList}>
+            <Link href="/standings">
+              <li>HOME</li>
+            </Link>
+            <Link href="/standings">
+              <li>CONTACT</li>
+            </Link>
+            <Link href="/standings">
+              <li>ABOUT</li>
+            </Link>
+          </ul>
+        </div>
+        <div className={classes["icon-title"]}>
+          <Image src={appIcon} alt="logo" />
+          <h1 className={classes.header}>Food2Door</h1>
+        </div>
+        <div className={classes.navbarButton}>
+          <BasketButton />
+        </div>
+      </nav>
+      <div className={classes["food-image"]}>
+        <Image src={bgImage} alt="foodImage" />
       </div>
-      <div className={classes["icon-title"]}>
-        <Image src={appIcon} alt="logo" />
-        <h1 className={classes.header}>Food2Door</h1>
-      </div>
-      <div className={classes.navbarButton}>
-        <BasketButton />
-      </div>
-    </nav>
+    </Fragment>
   );
 };
 
